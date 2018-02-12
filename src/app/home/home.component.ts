b5010fbd408406e9c1fd73b55f3a57fcf0577db2
John Duncan <john@johnduncanphoto.com>
2018-02-12 14:36:28 +0000

    imports added to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from "@angular/core";
+import { Observable } from 'rxjs';
+import { NgModule } from '@angular/core';
+import { ServiceService } from '../service.service'
 
 @Component({
   selector: 'app-home',
