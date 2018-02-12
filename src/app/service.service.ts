f1038813a48f908d53126d15ff1c4bbedba7d837
John Duncan <john@johnduncanphoto.com>
2018-02-12 14:34:28 +0000

    imports added in service

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HomeComponent } from './home/home.component';
 
 @Injectable()
 export class ServiceService {
