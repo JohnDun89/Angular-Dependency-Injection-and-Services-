fd19ce82da9519fda44c300a9dca63f366d8e88f
John Duncan <john@johnduncanphoto.com>
2018-02-12 14:18:51 +0000

    basic function added to service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from './app.component';
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}]
 
 
 @NgModule({
@@ -12,7 +20,11 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ServiceService {
 
   constructor() { }
 
+  upperCase(term): Observable<string> {
+   const returnValue = term.toUpperCase;
+    return returnValue;
+  }
 }
